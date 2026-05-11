5586f5f5a1a34485d1f42383fb99861d5102b07b
Martin Guerra Local <vernonguerra27@gmail.com>
2026-05-11 10:40:47 -0300

    ADD: aniado mas recursos en material de estudio

--- src/pages/AulaVirtualPage.tsx
+++ src/pages/AulaVirtualPage.tsx
@@ -5,12 +5,78 @@ const recursos = [
     tipo: "PDF",
     link: "/materiales/logaritmos.pdf",
   },
-//   {
-//     id: 2,
-//     titulo: "Ecuaciones",
-//     tipo: "Video",
-//     link: "/videos/video4EOG.mp4",
-//   },
+  {
+    id: 2,
+    titulo: "Ejercicios función lineal",
+    tipo: "PDF",
+    link: "/materiales/ejercicio_funcion_lineal.pdf",
+  },
+  {
+    id: 3,
+    titulo: "Ejercicios con racionales",
+    tipo: "PDF",
+    link: "/materiales/ejercicios_con_racionales.pdf",
+  },
+  {
+    id: 4,
+    titulo: "Expresiones algebraicas racionales",
+    tipo: "PDF",
+    link: "/materiales/expresiones_algebraicas_racionales.pdf",
+  },
+  {
+    id: 5,
+    titulo: "Expresiones algebraicas fraccionarias",
+    tipo: "PDF",
+    link: "/materiales/expresiones_algebricas_fraccionarias.pdf",
+  },
+  { 
+    id: 6,
+    titulo: "Factoreo",
+    tipo: "PDF",
+    link: "/materiales/factoreo.pdf",
+  },
+  {
+    id: 7,
+    titulo: "Funcion exponencial",
+    tipo: "PDF",
+    link: "/materiales/funcion_exponencial.pdf",
+  },
+  {
+    id: 8,
+    titulo: "Funciones trigonometricas",
+    tipo: "PDF",
+    link: "/materiales/funciones_trigonometricas.pdf",
+  },
+  {
+    id: 9,
+    titulo: "Identidades trigonométricas",
+    tipo: "PDF",
+    link: "/materiales/identidades_trigonometricas.pdf",
+  },
+  {
+    id: 10,
+    titulo: "Ejercicios logarítmicos",
+    tipo: "PDF",
+    link: "/materiales/logaritmo_ejercicio.pdf",
+  },
+  {
+    id: 11,
+    titulo: "numeros imaginarios",
+    tipo: "PDF",
+    link: "/materiales/nros_imaginarios.pdf",
+  },
+  {
+    id: 12,
+    titulo: "Numeros naturales",
+    tipo: "PDF",
+    link: "/materiales/numeros_naturales.pdf",
+  },
+  {
+    id: 13,
+    titulo: "Numeros reales",
+    tipo: "PDF",
+    link: "/materiales/numeros_reales.pdf",
+  }
 ];
 
 export default function AulaVirtualPage() {
